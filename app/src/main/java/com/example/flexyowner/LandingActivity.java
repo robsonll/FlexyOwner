7807765407c6b1e589e157d719b3f0237060b662
Robson Lourenco <robson.lourenco@gmail.com>
2020-12-03 22:30:53 -0800

    Updating gradle file

--- app/src/main/java/com/example/flexyowner/LandingActivity.java
+++ app/src/main/java/com/example/flexyowner/LandingActivity.java
@@ -23,10 +23,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
